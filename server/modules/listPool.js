f35ea83b60bc46f7f5f4eb6761702afed56bac9a
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-22 15:39:32 -0500

    updated pool module for heroku

--- server/modules/listPool.js
+++ server/modules/listPool.js
@@ -1,12 +1,30 @@
+
 const pg = require('pg');
+let pool;
 
+// When our app is deployed to the internet 
+// we'll use the DATABASE_URL environment variable
+// to set the connection info: web address, username/password, db name
+// eg: 
+if (process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+// When we're running this app on our own computer
+// we'll connect to the postgres database that is 
+// also running on our computer (localhost)
+else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'todo_list', 
+    });
+}
 
-//create link to sql database
-const pool = new pg.Pool({
-    database: 'todo_list',
-    host: 'localhost',
-    port: 5432
-});
-
+module.exports = pool;
 
 module.exports = pool;
